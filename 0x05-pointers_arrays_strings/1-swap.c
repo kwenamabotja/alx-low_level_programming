4f4707aed5779a1e23abbd6f2a9f6854ce1f49d8
Kwena <koena93@gmail.com>
2022-09-21 00:38:50 +0200

    empty readme

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,17 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * swap_int - this functoin change the initial.
- * @a: this is a parameter to change.
- * @b: this is the another parameter to change.
+ * swap_int - swaps the value of int a and int b
+ * @a: first int to swap
+ * @b: second int to swap
+ *
+ * Return: void
  */
 void swap_int(int *a, int *b)
 {
-	int t = *a;
+	int c;
+
+	c = *a;
 	*a = *b;
-	*b = t;
+	*b = c;
 }
